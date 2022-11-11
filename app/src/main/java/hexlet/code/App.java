8f0b43e75da583e3facb953a1d82c34a3fd77a24
Alexey Kalendarev <akalendarjr@gmail.com>
2022-11-11 11:52:14 +0300

    add code in App.java menu Wilcome to the Brain Games from Cli.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,31 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+        Scanner scannerChoice = new Scanner(System.in);
+        System.out.print("Your choice: ");
+        int choice = scannerChoice.nextInt();
+
+        if (choice == 1) {
+            System.out.println();
+            System.out.println("Welcome to the Brain Games!");
+            Cli.main(args);
+        } else {
+            return;
+        }
+
+        if (choice == 2) {
+            Even.main(args);
+        }
+
+
+        scannerChoice.close();
+
     }
 }
