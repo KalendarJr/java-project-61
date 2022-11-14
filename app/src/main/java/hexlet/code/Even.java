8c1027348a207d6a702a2efdac6868828274ed8b
Alexey Kalendarev <akalendarjr@gmail.com>
2022-11-14 15:53:08 +0300

    add file Even.java with the game 2 - Even

--- app/src/main/java/hexlet/code/Even.java
+++ app/src/main/java/hexlet/code/Even.java
@@ -1,2 +1,70 @@
-package hexlet.code;public class Even {
+package hexlet.code;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Even {
+    public static void main(String[] str) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("May I have your name? ");
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+
+        Random integerNumber = new Random();
+        int numberOne = integerNumber.nextInt(100);
+
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+        System.out.println("Question: " + numberOne);
+        System.out.print("Your answer: ");
+        String answerOne = scanner.next();
+
+        String yes = "yes";
+        String no = "no";
+
+        if (numberOne % 2 == 0 && answerOne.equals(yes)) {
+            System.out.println("Correct!");
+        } else if (numberOne / 2 != 0 && answerOne.equals(no)) {
+            System.out.println("Correct!");
+        } else {
+            System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.");
+            System.out.println("Let's try again, " + userName + "!");
+        }
+
+        int numberTwo = integerNumber.nextInt(100);
+
+        System.out.println("Question: " + numberTwo);
+        System.out.print("Your answer: ");
+        String answerTwo = scanner.next();
+
+        if (numberTwo % 2 == 0 && answerTwo.equals(yes)) {
+            System.out.println("Correct!");
+        } else if (numberTwo / 2 != 0 && answerTwo.equals(no)) {
+            System.out.println("Correct!");
+        } else {
+            System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.");
+            System.out.println("Let's try again, " + userName + "!");
+            return;
+        }
+
+        int numberThree = integerNumber.nextInt(100);
+
+        System.out.println("Question: " + numberThree);
+        System.out.print("Your answer: ");
+        String answerThree = scanner.next();
+
+        if (numberThree % 2 == 0 && answerThree.equals(yes)) {
+            System.out.println("Correct!");
+            System.out.println("Congratulations, " + userName + "!");
+        } else if (numberThree / 2 != 0 && answerThree.equals(no)) {
+            System.out.println("Correct!");
+            System.out.println("Congratulations, " + userName + "!");
+        } else {
+            System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.");
+            System.out.println("Let's try again, " + userName + "!");
+            return;
+        }
+
+        scanner.close();
+    }
 }
