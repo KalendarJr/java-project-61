caa8c5d380bfaba65f0994ca4243fefaa1fcc6af
Alexey Kalendarev <akalendarjr@gmail.com>
2022-11-14 15:52:24 +0300

    add in file App.java game selection 2 - Even

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -8,22 +8,26 @@ public class App {
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
         System.out.println("0 - Exit");
+
         Scanner scannerChoice = new Scanner(System.in);
+
         System.out.print("Your choice: ");
+
         int choice = scannerChoice.nextInt();
 
         if (choice == 1) {
             System.out.println();
             System.out.println("Welcome to the Brain Games!");
             Cli.main(args);
-        } else {
-            return;
         }
 
         if (choice == 2) {
+            System.out.println();
+            System.out.println("Welcome to the Brain Games!");
             Even.main(args);
+        } else {
+            return;
         }
-
 
         scannerChoice.close();
 
