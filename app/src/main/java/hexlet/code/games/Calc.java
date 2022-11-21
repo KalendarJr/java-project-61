c093b26540d589c921e82bc9b211f13c51559f49
Alexey Kalendarev <akalendarjr@gmail.com>
2022-11-21 15:13:50 +0300

    change code in file Calc.java for Engine games

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -1,70 +1,47 @@
-package hexlet.code;
+package hexlet.code.games;
 
-import java.util.Random;
-import java.util.Scanner;
-
+import hexlet.code.Engine;
 public class Calc {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("May I have your name? ");
-        String userName = scanner.next();
-        System.out.println("Hello, " + userName + "!");
-
-        Random numberRandom = new Random();
-        int numberOne = numberRandom.nextInt(20);
-        int numberTwo = numberRandom.nextInt(10);
+        String userName = Engine.nameUser();
 
         System.out.println("What is the result of the expression?");
-        System.out.println("Question: " + numberOne + " * " + numberTwo);
+        System.out.println("Question: " + Engine.numberOne + " * " + Engine.numberTwo);
         System.out.print("Your answer: ");
-        int answerOne = scanner.nextInt();
+        int resultOne = Engine.answerUser();
 
-        var resultOne = numberOne * numberTwo;
-
-        if (answerOne == resultOne) {
+        if (resultOne == Engine.resultOne) {
             System.out.println("Correct!");
         } else {
-            System.out.println("'" + answerOne + "'" + " is wrong answer ;(. Correct answer was " + "'" + resultOne + "'.");
+            System.out.println("'" + resultOne + "'" + " is wrong answer ;(. Correct answer was " + "'" + Engine.resultOne + "'.");
             System.out.println("Let's try again, " + userName + "!");
             return;
         }
 
-        int numberThree = numberRandom.nextInt(50);
-        int numberFour = numberRandom.nextInt(50);
-
-        System.out.println("Question: " + numberThree + " + " + numberFour);
+        System.out.println("Question: " + Engine.numberThree + " + " + Engine.numberFour);
         System.out.print("Your answer: ");
-        int answerTwo = scanner.nextInt();
+        int resultTwo = Engine.answerUser();
 
-        var resultTwo = numberThree + numberFour;
-
-        if (answerTwo == resultTwo) {
+        if (resultTwo == Engine.resultTwo) {
             System.out.println("Correct!");
         } else {
-            System.out.println("'" + answerTwo + "'" + " is wrong answer ;(. Correct answer was " + "'" + resultTwo + "'.");
+            System.out.println("'" + resultTwo + "'" + " is wrong answer ;(. Correct answer was " + "'" + Engine.resultTwo + "'.");
             System.out.println("Let's try again, " + userName + "!");
             return;
         }
 
-        int numberFive = numberRandom.nextInt(50);
-        int numberSix = numberRandom.nextInt(500);
-
-        System.out.println("Question: " + numberFive + " - " + numberSix);
+        System.out.println("Question: " + Engine.numberFive + " - " + Engine.numberSix);
         System.out.print("Your answer: ");
-        int answerThree = scanner.nextInt();
 
-        var resultThree = numberFive - numberSix;
+        int resultThree = Engine.answerUser();
 
-        if (answerThree == resultThree) {
+        if (resultThree == Engine.resultThree) {
             System.out.println("Correct!");
             System.out.println("Congratulations, " + userName + "!");
         } else {
-            System.out.println("'" + answerThree + "'" + " is wrong answer ;(. Correct answer was " + "'" + resultThree + "'.");
+            System.out.println("'" + resultThree + "'" + " is wrong answer ;(. Correct answer was " + "'" + Engine.resultThree + "'.");
             System.out.println("Let's try again, " + userName + "!");
-            return;
         }
 
-        scanner.close();
     }
 }
