9b73299d605b52ce352d85c6aec35ebce069f0d5
Alexey Kalendarev <akalendarjr@gmail.com>
2022-11-28 16:09:24 +0300

    add 4 menu for games GCD

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 import hexlet.code.games.Calc;
 import hexlet.code.games.Even;
+import hexlet.code.games.GCD;
 
 import java.util.Scanner;
 
@@ -11,6 +12,7 @@ public class App {
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
         System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
         System.out.println("0 - Exit");
 
         Scanner scannerChoice = new Scanner(System.in);
@@ -35,6 +37,12 @@ public class App {
             System.out.println();
             System.out.println("Welcome to the Brain Games!");
             Calc.main(args);
+        }
+
+        if (choice == 4) {
+            System.out.println();
+            System.out.println("Welcome to the Brain Games!");
+            GCD.main(args);
         }
 
         scannerChoice.close();
