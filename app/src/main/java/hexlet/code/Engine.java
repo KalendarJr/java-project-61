e904fc83c570213839eefc45fff51f2a6c079f41
Alexey Kalendarev <akalendarjr@gmail.com>
2022-12-07 13:43:02 +0300

    update code in Engine.java and add code for game Progression

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,12 +1,24 @@
 package hexlet.code;
 
+import org.apache.commons.lang3.RandomUtils;
+
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Engine {
+    public static int oneNumber = RandomUtils.nextInt(2, 10);
+    public static int twoNumber = RandomUtils.nextInt(3, 10);
+    public static int threeNumber = RandomUtils.nextInt(1, 11);
+    public static int progressionOneNumber = RandomUtils.nextInt(20, 28);
+    public static int progressionTwoNumber = RandomUtils.nextInt(20, 35);
+    public static int progressionThreeNumber = RandomUtils.nextInt(30, 50);
     public static Random numberRandom = new Random();
     public static int numberOne = numberRandom.nextInt(20);
-    public static int numberTwo = numberRandom.nextInt(10);
+    public static int numberTwo = numberRandom.nextInt(16);
+    public static int resultOneNumber = oneNumber + 8;
+    public static int resultTwoNumber = twoNumber + 6;
+    public static int resultThreeNumber = threeNumber + 15;
     public static int numberThree = numberRandom.nextInt(50);
     public static int numberFour = numberRandom.nextInt(50);
     public static int numberFive = numberRandom.nextInt(50);
@@ -14,8 +26,23 @@ public class Engine {
     public static int resultOne = numberOne * numberTwo;
     public static int resultTwo = numberThree + numberFour;
     public static int resultThree = numberFive - numberSix;
+    public static int resultFour = numberTwo - 2;
 
-      public static String nameUser() {
+    public static String question = "Question: ";
+//    public static String yourAnswer = "Your answer: ";
+    public static String letsTry = "Let's try again, ";
+    public static String isWrong = " is wrong answer ;(. Correct answer was '";
+//    public static String questionEven = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+//    public static String questionCalc = "What is the result of the expression?";
+//    public static String questionGCD = "Find the greatest common divisor of given numbers.";
+    public static String questionProgression = "What number is missing in the progression?";
+
+
+    public static void welcomeUser() {
+        System.out.println();
+        System.out.println("Welcome to the Brain Games!");
+    }
+    public static String nameUser() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("May I have your name? ");
         String userName = scanner.next();
@@ -25,8 +52,8 @@ public class Engine {
 
     public static String answerUserString() {
         Scanner scanner = new Scanner(System.in);
-        String answer = scanner.next();
-        return answer;
+        String answerString = scanner.next();
+        return answerString;
     }
 
      public static int answerUser() {
@@ -35,18 +62,39 @@ public class Engine {
          return answer;
      }
 
-    public static int randomMap(int number) {
-        Random numberRandom = new Random();
-        int numberOne = numberRandom.nextInt(20);
-        int numberTwo = numberRandom.nextInt(10);
-        int numberThree = numberRandom.nextInt(50);
-        int numberFour = numberRandom.nextInt(50);
-        int numberFive = numberRandom.nextInt(50);
-        int numberSix = numberRandom.nextInt(500);
-        int resultOne = numberOne * numberTwo;
-        int resultTwo = numberThree + numberFour;
-        int resultThree = numberFive - numberSix;
+     public static void progressionOne() {
+          for (var i = oneNumber; i < progressionOneNumber; i+=2) {
+              if(i == resultOneNumber) {
+                  System.out.print(".. ");
+                  continue;
+              }
+              System.out.print(i + " ");
+          }
+         System.out.println();
+         System.out.print("Your answer: ");
+     }
 
-        return number;
+    public static void progressionTwo() {
+        for (var i = twoNumber; i < progressionTwoNumber; i+=3) {
+            if(i == resultTwoNumber) {
+                System.out.print(".. ");
+                continue;
+            }
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        System.out.print("Your answer: ");
+    }
+
+    public static void progressionThree() {
+        for (var i = threeNumber; i < progressionThreeNumber; i+=5) {
+            if(i == resultThreeNumber) {
+                System.out.print(".. ");
+                continue;
+            }
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        System.out.print("Your answer: ");
     }
 }
