28f781d7cd0b891a8a33454a88c1e94f32f3fdca
Alexey Kalendarev <akalendarjr@gmail.com>
2023-01-18 18:28:14 +0300

    change code in GCD.java using method maxGcd

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -12,38 +12,40 @@ public class GCD {
         System.out.print("Your answer: ");
         int resultOne = Engine.answerUser();
 
-        if (resultOne == Engine.numberOne) {
+        var oneGcd = Engine.maxGcd(Engine.numberOne, Engine.numberOne * i);
+        if (resultOne == Engine.maxGcd(Engine.numberOne, Engine.numberOne * i)) {
             System.out.println("Correct!");
         } else {
-            System.out.println("'" + resultOne + "'" + " is wrong answer ;(. Correct answer was '" + i + "'.");
+            System.out.println("'" + resultOne + "'" + " is wrong answer ;(. Correct answer was '" + oneGcd + "'.");
             System.out.println("Let's try again, " + userName + "!");
             return;
         }
 
         var j = 5;
-        System.out.println("Question: " + (Engine.numberTwo * j + 30) + " " + (Engine.numberTwo + 30));
+        System.out.println("Question: " + (Engine.numberTwo * j) + " " + (Engine.numberTwo + 30));
         System.out.print("Your answer: ");
         int resultTwo = Engine.answerUser();
 
-        if (resultTwo == Engine.numberTwo) {
+        var twoGcd = Engine.maxGcd(Engine.numberTwo * j, Engine.numberTwo + 30);
+        if (resultTwo == twoGcd) {
             System.out.println("Correct!");
         } else {
-            System.out.println("'" + resultTwo + "'" + " is wrong answer ;(. Correct answer was " + "'" + Engine.numberTwo + "'.");
+            System.out.println("'" + resultTwo + "'" + " is wrong answer ;(. Correct answer was " + "'" + twoGcd + "'.");
             System.out.println("Let's try again, " + userName + "!");
             return;
         }
 
         var k = 7;
-        System.out.println("Question: " + Engine.numberThree + " " + Engine.numberThree * k);
+        System.out.println("Question: " + Engine.numberOne + " " + Engine.numberOne * k);
         System.out.print("Your answer: ");
-
         int resultThree = Engine.answerUser();
 
-        if (resultThree == Engine.numberThree) {
+        var threeGcd = Engine.maxGcd(Engine.numberOne, Engine.numberOne * k);
+        if (resultThree == threeGcd) {
             System.out.println("Correct!");
             System.out.println("Congratulations, " + userName + "!");
         } else {
-            System.out.println("'" + resultThree + "'" + " is wrong answer ;(. Correct answer was " + "'" + Engine.numberThree + "'.");
+            System.out.println("'" + resultThree + "'" + " is wrong answer ;(. Correct answer was " + "'" + threeGcd + "'.");
             System.out.println("Let's try again, " + userName + "!");
         }
     }
